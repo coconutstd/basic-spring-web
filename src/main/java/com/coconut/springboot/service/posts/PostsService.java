fd7301a6a862300bc445fed34834a294b3f552b9
junui <wnsspdl@naver.com>
2021-01-29 10:56:17 +0900

    IndexController PostsListResponseDto PostsRepository PostsService 생성 및 변경

--- src/main/java/com/coconut/springboot/service/posts/PostsService.java
+++ src/main/java/com/coconut/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.coconut.springboot.service.posts;
 
 import com.coconut.springboot.domain.posts.Posts;
 import com.coconut.springboot.domain.posts.PostsRepository;
+import com.coconut.springboot.web.dto.PostsListResponseDto;
 import com.coconut.springboot.web.dto.PostsResponseDto;
 import com.coconut.springboot.web.dto.PostsSaveRequestDto;
 import com.coconut.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +31,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
