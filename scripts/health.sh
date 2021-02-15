c7d2624340200a7866bfbe5e4db71287fb76e1a8
junui <wnsspdl@naver.com>
2021-02-15 17:21:03 +0900

    health.sh 오타 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(reallink -f $0)
+ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
-IDLE_PORT=$(find_idel_port)
+IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
@@ -18,7 +18,7 @@ do
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
       break
@@ -27,9 +27,9 @@ do
       echo "> Health check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10]
+  if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health 실패. "
+    echo "> Health check 실패. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
@@ -37,5 +37,3 @@ do
   echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
-
-
